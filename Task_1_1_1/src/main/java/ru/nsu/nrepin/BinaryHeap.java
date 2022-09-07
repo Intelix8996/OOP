e2667ad908f1c00f447f4eb57e184377a0ea80bc
Intelix8996 <nikolat-repin201303@mail.ru>
2022-09-07 23:58:51 +0700

    Add javadoc comments

--- Task_1_1_1/src/main/java/ru/nsu/nrepin/BinaryHeap.java
+++ Task_1_1_1/src/main/java/ru/nsu/nrepin/BinaryHeap.java
@@ -1,35 +1,69 @@
 package ru.nsu.nrepin;
 
+/**
+ * Class that represents binary heap
+ */
 public class BinaryHeap {
 
+    /**
+     * The size of the heap
+     */
     private int heapSize;
+    /**
+     * Buffer for the heap
+     */
     private int[] array;
 
+    /**
+     * @return heap size
+     */
     public int getHeapSize() {
         return heapSize;
     }
 
+    /**
+     * Set size of the heap
+     *
+     * @param heapSize new heap size
+     */
     public void setHeapSize(int heapSize) {
         this.heapSize = heapSize;
     }
 
+    /**
+     * Builds binary heap on given array
+     *
+     * @param array array to build heap on
+     */
     public BinaryHeap(int[] array) {
 
         this.array = array;
 
         this.heapSize = array.length;
 
-        for (int i = this.heapSize / 2 - 1; i >= 0; --i){
+        for (int i = this.heapSize / 2 - 1; i >= 0; --i) {
             siftDown(i);
         }
     }
 
-    public void swap(int i, int j){
+    /**
+     * Swaps two given elements i and j of the buffer
+     *
+     * @param i - first element
+     * @param j - second element
+     */
+    public void swap(int i, int j) {
         int t = array[i];
         array[i] = array[j];
         array[j] = t;
     }
-    public void siftDown(int i){
+
+    /**
+     * Perform sift down on the heap from a given index i
+     *
+     * @param i - start element
+     */
+    public void siftDown(int i) {
 
         int maxVal;
         int maxChild;
@@ -43,13 +77,11 @@ public class BinaryHeap {
         if (right > heapSize - 1) {
             maxVal = array[left];
             maxChild = left;
-        }
-        else {
+        } else {
             if (array[left] >= array[right]) {
                 maxVal = array[left];
                 maxChild = left;
-            }
-            else {
+            } else {
                 maxVal = array[right];
                 maxChild = right;
             }
