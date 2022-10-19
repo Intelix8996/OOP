18338f616d7d7c1b117f7c4b2e4f67f575d7b9a6
Intelix8996 <nikolat-repin201303@mail.ru>
2022-10-19 20:59:40 +0700

    Implement AdjacencyListsGraph, IncidenceMatrixGraph and Pair

---
+++ Task_1_2_3/src/main/java/ru/nsu/nrepin/AdjacencyListsGraph.java
@@ -0,0 +1,135 @@
+package ru.nsu.nrepin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AdjacencyListsGraph<V, W extends Number> implements Graph<V, W> {
+
+    private final Map<Node<V>, List<Pair<Node<V>, Edge<W>>>> nodes;
+
+    public AdjacencyListsGraph() {
+        nodes = new HashMap<>();
+    }
+
+    @Override
+    public Edge<W> addEdge(Node<V> u, Node<V> v, W weight) {
+        Edge<W> newEdge = new Edge<>(weight);
+
+        nodes.get(u).add(new Pair<>(v, newEdge));
+
+        return newEdge;
+    }
+
+    @Override
+    public Edge<W> getEdge(Node<V> u, Node<V> v) {
+        for (var pair : nodes.get(u)) {
+            if (pair.getLeft().equals(v)) {
+                return pair.getRight();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Edge<W>> getNodeEdges(Node<V> u) {
+
+        List<Edge<W>> edges = new ArrayList<>();
+
+        for (var pair : nodes.get(u)) {
+            edges.add(pair.getRight());
+        }
+
+        return edges;
+    }
+
+    @Override
+    public Node<V> getEdgeTerminus(Node<V> u, Edge<W> e) {
+        for (var pair : nodes.get(u)) {
+            if (pair.getRight().equals(e)) {
+                return pair.getLeft();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Edge<W>> getEdges() {
+        List<Edge<W>> edges = new ArrayList<>();
+
+        for (var node : nodes.keySet()) {
+            for (var pair : nodes.get(node)) {
+                edges.add(pair.getRight());
+            }
+        }
+
+        return edges;
+    }
+
+    @Override
+    public boolean removeEdge(Node<V> u, Node<V> v) {
+        for (var pair : nodes.get(u)) {
+            if (pair.getLeft().equals(v)) {
+                nodes.get(u).remove(pair);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Node<V> addVertex(V value) {
+        Node<V> newNode = new Node<>(value);
+
+        nodes.put(newNode, new ArrayList<>());
+
+        return newNode;
+    }
+
+    @Override
+    public List<Node<V>> getVertices() {
+        return nodes.keySet().stream().toList();
+    }
+
+    @Override
+    public boolean removeVertex(Node<V> node) {
+        if (!nodes.containsKey(node)) {
+            return false;
+        }
+
+        nodes.remove(node);
+
+        return true;
+    }
+
+    @Override
+    public Node<V> find(V value) {
+        for (var node : nodes.keySet()) {
+            if (node.getValue().equals(value)) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getVertexCount() {
+        return nodes.keySet().size();
+    }
+
+    @Override
+    public int getEdgesCount() {
+
+        int edgesCount = 0;
+
+        for (var node : nodes.keySet()) {
+            edgesCount += nodes.get(node).size();
+        }
+
+        return edgesCount;
+    }
+}
---
+++ Task_1_2_3/src/main/java/ru/nsu/nrepin/IncidenceMatrixGraph.java
@@ -0,0 +1,166 @@
+package ru.nsu.nrepin;
+
+import java.util.*;
+
+public class IncidenceMatrixGraph<V, W extends Number> implements Graph<V, W> {
+
+    private final Map<Node<V>, Map<Edge<W>, EdgeDirection>> incidenceMatrix;
+
+    private enum EdgeDirection {
+        INGOING,
+        OUTGOING,
+        NO_EDGE
+    }
+
+    public IncidenceMatrixGraph() {
+        incidenceMatrix = new HashMap<>();
+    }
+
+    @Override
+    public Edge<W> addEdge(Node<V> u, Node<V> v, W weight) {
+        Edge<W> newEdge = new Edge<>(weight);
+
+        for (var node : incidenceMatrix.keySet()) {
+            incidenceMatrix.get(node).put(newEdge, EdgeDirection.NO_EDGE);
+        }
+
+        incidenceMatrix.get(u).put(newEdge, EdgeDirection.OUTGOING);
+        incidenceMatrix.get(v).put(newEdge, EdgeDirection.INGOING);
+
+        return newEdge;
+    }
+
+    @Override
+    public Edge<W> getEdge(Node<V> u, Node<V> v) {
+
+        for (var edge : incidenceMatrix.get(u).keySet()) {
+            if (incidenceMatrix.get(u).get(edge) == EdgeDirection.OUTGOING &&
+                    incidenceMatrix.get(v).get(edge) == EdgeDirection.INGOING) {
+                return edge;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Edge<W>> getNodeEdges(Node<V> u) {
+
+        List<Edge<W>> edges = new ArrayList<>();
+
+        for (var edge : incidenceMatrix.get(u).keySet()) {
+            if (incidenceMatrix.get(u).get(edge) == EdgeDirection.OUTGOING) {
+                edges.add(edge);
+            }
+        }
+
+        return edges;
+    }
+
+    @Override
+    public Node<V> getEdgeTerminus(Node<V> u, Edge<W> e) {
+
+        if (incidenceMatrix.get(u).get(e) == EdgeDirection.NO_EDGE) {
+            return null;
+        }
+
+        for (var node : incidenceMatrix.keySet()) {
+            if (incidenceMatrix.get(node).get(e) == EdgeDirection.INGOING) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Edge<W>> getEdges() {
+        if (getVertexCount() > 0) {
+            Set<Edge<W>> edges = new HashSet<>();
+
+            for (var node : incidenceMatrix.keySet()) {
+                edges = incidenceMatrix.get(node).keySet();
+                break;
+            }
+
+            return edges.stream().toList();
+        }
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean removeEdge(Node<V> u, Node<V> v) {
+
+        Edge<W> newEdge = getEdge(u, v);
+
+        if (newEdge == null) {
+            return false;
+        }
+
+        incidenceMatrix.get(u).put(newEdge, EdgeDirection.NO_EDGE);
+        incidenceMatrix.get(v).put(newEdge, EdgeDirection.NO_EDGE);
+
+        return true;
+    }
+
+    @Override
+    public Node<V> addVertex(V value) {
+        Node<V> newNode = new Node<>(value);
+
+        incidenceMatrix.put(newNode, new HashMap<>());
+
+        if (incidenceMatrix.size() > 1) {
+            Set<Edge<W>> edges = new HashSet<>();
+
+            for (var node : incidenceMatrix.keySet()) {
+                if (!node.equals(newNode)) {
+                    edges = incidenceMatrix.get(node).keySet();
+                    break;
+                }
+            }
+
+            for (var edge : edges) {
+                incidenceMatrix.get(newNode).put(edge, EdgeDirection.NO_EDGE);
+            }
+        }
+
+        return newNode;
+    }
+
+    @Override
+    public List<Node<V>> getVertices() {
+        return new ArrayList<>(incidenceMatrix.keySet());
+    }
+
+    @Override
+    public boolean removeVertex(Node<V> node) {
+        if (incidenceMatrix.get(node) == null) {
+            return false;
+        }
+
+        incidenceMatrix.remove(node);
+
+        return true;
+    }
+
+    @Override
+    public Node<V> find(V value) {
+        for (var node : incidenceMatrix.keySet()) {
+            if (node.getValue().equals(value)) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getVertexCount() {
+        return incidenceMatrix.keySet().size();
+    }
+
+    @Override
+    public int getEdgesCount() {
+        return getEdges().size();
+    }
+}
