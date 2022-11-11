f3a27f560a5509e4722d9aa79e4b87cc66eb8ac6
Nikolay Repin <nikolat-repin201303@mail.ru>
2022-11-11 21:24:30 +0700

    Implement Task_1_3_1

---
+++ Task_1_3_1/src/test/java/ru/nsu/nrepin/StringFinderTests.java
@@ -0,0 +1,103 @@
+package ru.nsu.nrepin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * Tester class for StringFinder.
+ */
+public class StringFinderTests {
+
+    /**
+     * Test a case when multiple occurrences of pattern.
+     */
+    @Test
+    public void testMultipleOccurrences() {
+        List<Integer> answer = null;
+
+        try {
+            answer = SubstringFinder.findSubstringsInFile("/test1.txt", "cake");
+        } catch (Exception ignore) { }
+
+        List<Integer> referenceList = List.of(9, 15, 21);
+
+        Assertions.assertEquals(referenceList, answer);
+    }
+
+    /**
+     * Test a case when occurrence of pattern.
+     */
+    @Test
+    public void testSingleOccurrence() {
+        List<Integer> answer = null;
+
+        try {
+            answer = SubstringFinder.findSubstringsInFile("/test2.txt", "cake");
+        } catch (Exception ignore) { }
+
+        List<Integer> referenceList = List.of(0);
+
+        Assertions.assertEquals(referenceList, answer);
+    }
+
+    /**
+     * Test a case when pattern is at the beginning of the file.
+     */
+    @Test
+    public void testPrefix() {
+        List<Integer> answer = null;
+
+        try {
+            answer = SubstringFinder.findSubstringsInFile("/test3.txt", "cake");
+        } catch (Exception ignore) { }
+
+        List<Integer> referenceList = List.of(24);
+
+        Assertions.assertEquals(referenceList, answer);
+    }
+
+    /**
+     * Test a case when pattern is at the end of the file.
+     */
+    @Test
+    public void testPostfix() {
+        List<Integer> answer = null;
+
+        try {
+            answer = SubstringFinder.findSubstringsInFile("/test4.txt", "cake");
+        } catch (Exception ignore) { }
+
+        List<Integer> referenceList = List.of(0);
+
+        Assertions.assertEquals(referenceList, answer);
+    }
+
+    /**
+     * Test a case when pattern absent in the file.
+     */
+    @Test
+    public void testNoOccurrence() {
+        List<Integer> answer = null;
+
+        try {
+            answer = SubstringFinder.findSubstringsInFile("/test5.txt", "cake");
+        } catch (Exception ignore) { }
+
+        List<Integer> referenceList = List.of();
+
+        Assertions.assertEquals(referenceList, answer);
+    }
+
+    /**
+     * Test a case when filename is wrong.
+     */
+    @Test
+    public void testWrongFilename() {
+        Assertions.assertThrows(
+                FileNotFoundException.class,
+                () -> SubstringFinder.findSubstringsInFile("file.txt", "cake"));
+    }
+}
