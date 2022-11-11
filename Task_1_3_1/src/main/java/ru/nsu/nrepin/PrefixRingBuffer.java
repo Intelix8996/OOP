f0474d0a22bed6e02c82bd41cb495415403c91d3
Nikolay Repin <nikolat-repin201303@mail.ru>
2022-11-11 21:40:27 +0700

    Fix codestyle

--- Task_1_3_1/src/main/java/ru/nsu/nrepin/PrefixRingBuffer.java
+++ Task_1_3_1/src/main/java/ru/nsu/nrepin/PrefixRingBuffer.java
@@ -1,27 +1,27 @@
 package ru.nsu.nrepin;
 
 /**
- * This class implements special type of buffer.
- * It is used to construct Z function of string with little amount of memory.
+ * <p>This class implements special type of buffer.
+ * It is used to construct Z function of string with little amount of memory.</p>
  *
- * First {@code prefixSize} [0; preifxSize-1] fields work as regular array.
+ * <p>First {@code prefixSize} [0; preifxSize-1] fields work as regular array.
  * Next {@code ringBufferSize} [ringBufferSize; +inf) work as ring buffer.
  * This allows data be written to any index, physically data will be stored in ring buffer.
  * Data is mapped to ring buffer by the following formula:
- * ring_buffer_index = ((index - prefixSize) % ringBufferSize) + prefixSize;
+ * ring_buffer_index = ((index - prefixSize) % ringBufferSize) + prefixSize;</p>
  *
- * Example:
- * let {@code zb} be {@code ZBuffer} with {@code prefixSize} = {@code ringBuffersize} = 5.
+ * <p>Example:
+ * let {@code zb} be {@code PrefixRingBuffer} with {@code prefixSize} = {@code ringBuffersize} = 5.
  * let {@code b} be zb's internal buffer.
  * Then:
  * [0; 4] will be regular array.
  * [5; 9] will be ring buffer.
  * zb[2] will point to b[2].
  * zb[5] will point to b[5].
- * zb[10] will point ot b[5].
- * zb[12] will point ti b[7].
+ * zb[10] will point to b[5].
+ * zb[12] will point to b[7].</p>
  */
-public class ZBuffer {
+public class PrefixRingBuffer {
 
     private final int prefixSize;
 
@@ -30,12 +30,12 @@ public class ZBuffer {
     private final int[] buffer;
 
     /**
-     * Creates new {@code ZBuffer}.
+     * Creates new {@code PrefixRingBuffer}.
      *
      * @param prefixSize size of regular array
      * @param ringBufferSize size of ring buffer
      */
-    public ZBuffer(int prefixSize, int ringBufferSize) {
+    public PrefixRingBuffer(int prefixSize, int ringBufferSize) {
         this.prefixSize = prefixSize;
         this.ringBufferSize = ringBufferSize;
         this.buffer = new int[prefixSize + ringBufferSize];
