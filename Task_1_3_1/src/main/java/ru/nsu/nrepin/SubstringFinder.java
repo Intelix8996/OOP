0cc23066b51166747a6728cd9f2ffcb24040315f
Nikolay Repin <nikolat-repin201303@mail.ru>
2022-11-23 19:57:46 +0700

    Generalize findSubstrings function and make algorithm run in linear time

--- Task_1_3_1/src/main/java/ru/nsu/nrepin/SubstringFinder.java
+++ Task_1_3_1/src/main/java/ru/nsu/nrepin/SubstringFinder.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,28 +14,9 @@ import java.util.List;
  */
 public class SubstringFinder {
 
-    /**
-     * Searches all occurrences of pattern in files.
-     * Returns their indices as {@code List<Integer>}.
-     *
-     * @param filename file
-     * @param pattern substring to search
-     * @return list of indices of substring occurrences
-     * @throws FileNotFoundException if file was not found
-     */
-    public static List<Integer> findSubstringsInFile(
-            String filename,
-            String pattern)
-            throws FileNotFoundException {
-
-        // Create BufferedReader
-        InputStream inputStream = SubstringFinder.class.getResourceAsStream(filename);
-
-        if (inputStream == null) {
-            throw new FileNotFoundException("Can't open file");
-        }
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+    public static List<Integer> findSubstrings(
+            Reader reader,
+            String pattern) {
 
         // Length of actual pattern and delimiter ('\0')
         int patternLength = pattern.length() + 1;
@@ -50,7 +32,51 @@ public class SubstringFinder {
         // Write prefix delimiter ('\0')
         buffer.set(patternLength - 1, '\0');
 
-        // Fill ring buffer region with beginning of file
+        int position = patternLength;
+
+        List<Integer> result = new ArrayList<>();
+
+        boolean endOfStream = false;
+
+        int matchLength = 0;
+
+        while (true) {
+
+            if (endOfStream) {
+                break;
+            }
+
+            int c;
+
+            try {
+                c = reader.read();
+            } catch (IOException e) {
+                break;
+            }
+
+            if (c == -1) {
+                endOfStream = true;
+            }
+
+            position++;
+
+            //buffer.set(position++, c);
+
+            if (buffer.get(matchLength) == c) {
+                matchLength++;
+            } else if (buffer.get(0) == c) {
+                matchLength = 1;
+            } else {
+                matchLength = 0;
+            }
+
+            if (matchLength == patternLength - 1) {
+                result.add(position - patternLength - matchLength);
+                matchLength = 0;
+            }
+        }
+
+        /*// Fill ring buffer region with beginning of file
         for (int i = 0; i < patternLength; ++i) {
             int c;
 
@@ -59,7 +85,12 @@ public class SubstringFinder {
             } catch (IOException e) {
                 break;
             }
+
             buffer.set(patternLength + i, c);
+
+            if (c == -1) {
+                break;
+            }
         }
 
         // Calculate starting position
@@ -102,8 +133,34 @@ public class SubstringFinder {
             }
 
             buffer.set(position++, c);
-        }
+        }*/
 
         return result;
+    }
+
+    /**
+     * Searches all occurrences of pattern in files.
+     * Returns their indices as {@code List<Integer>}.
+     *
+     * @param filename file
+     * @param pattern substring to search
+     * @return list of indices of substring occurrences
+     * @throws FileNotFoundException if file was not found
+     */
+    public static List<Integer> findSubstringsInFile(
+            String filename,
+            String pattern)
+            throws FileNotFoundException {
+
+        // Create BufferedReader
+        InputStream inputStream = SubstringFinder.class.getResourceAsStream(filename);
+
+        if (inputStream == null) {
+            throw new FileNotFoundException("Can't open file");
+        }
+
+        Reader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        return findSubstrings(reader, pattern);
     }
 }
