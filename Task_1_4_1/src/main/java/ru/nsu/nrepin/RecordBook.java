dbf67da727d11503ba7b13d0162bdd007797c6cf
Nikolay Repin <nikolat-repin201303@mail.ru>
2022-12-01 00:39:40 +0700

    Implement canHaveHonoursDegree method and add tests

--- Task_1_4_1/src/main/java/ru/nsu/nrepin/RecordBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/nrepin/RecordBook.java
@@ -5,8 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class implements student's record book.
+ */
 public class RecordBook {
 
+    /**
+     * Define possible grades.
+     */
     public enum Grade {
         POOR(2),
         SATISFACTORY(3),
@@ -19,6 +25,11 @@ public class RecordBook {
             value = intValue;
         }
 
+        /**
+         * Return integer equivalent of a grade.
+         *
+         * @return integer mark
+         */
         public int intValue() {
             return value;
         }
@@ -30,28 +41,64 @@ public class RecordBook {
 
     private final int semesterCount;
 
+    private Grade qualificationWorkMark;
+
+    private boolean qualificationWorkDone;
+
+    /**
+     * Constructs new RecordBook with given semester count.
+     *
+     * @param semesterCount number of semesters.
+     */
     public RecordBook(int semesterCount) {
         semesters = new ArrayList<>(semesterCount);
         this.semesterCount = semesterCount;
+        qualificationWorkDone = false;
 
         for (int i = 0; i < semesterCount; ++i) {
             semesters.add(new HashMap<>());
         }
     }
 
+    /**
+     * Constructs new RecordBook with default (8 for bachelor's degree) semester count.
+     */
     public RecordBook() {
         this(DEFAULT_SEMESTER_COUNT);
     }
 
-    public void addMark(int semester, String subject, Grade mark) {
-        if (semester > semesterCount) {
+    /**
+     * Adds new subject to given semester and puts mark there.
+     * <i>Note: semester counts from 1</i>.
+     *
+     * @param semester subject semester
+     * @param subject subject name
+     * @param mark mark to put
+     */
+    public void putMark(int semester, String subject, Grade mark) {
+        if (semester > semesterCount || semester < 1) {
             throw new IndexOutOfBoundsException();
         }
 
         semesters.get(semester - 1).put(subject, mark);
     }
 
-    public float averageScore() {
+    /**
+     * Rates qualification work.
+     *
+     * @param mark qualification work mark
+     */
+    public void putQualificationWorkMark(Grade mark) {
+        qualificationWorkDone = true;
+        qualificationWorkMark = mark;
+    }
+
+    /**
+     * Get an average score.
+     *
+     * @return average score.
+     */
+    public float getAverageScore() {
         float score = 0;
         int marksCount = 0;
 
@@ -65,8 +112,14 @@ public class RecordBook {
         return score / marksCount;
     }
 
+    /**
+     * Check if student can have enhanced stipend in given semester.
+     *
+     * @param semester semester to check
+     * @return {@code true} if student can have an enhanced stipend, {@code false} otherwise
+     */
     public boolean canHaveEnhancedStipend(int semester) {
-        if (semester > semesterCount) {
+        if (semester > semesterCount || semester < 1) {
             throw new IndexOutOfBoundsException();
         }
 
@@ -84,5 +137,30 @@ public class RecordBook {
         }
 
         return answer;
+    }
+
+    /**
+     * Check if student can get a degree with honour.
+     *
+     * @return {@code true} if student can get a degree with honour, {@code false} otherwise
+     */
+    public boolean canHaveHonoursDegree() {
+        for (Map<String, Grade> semester : semesters) {
+            for (Grade grade : semester.values()) {
+                if (grade.intValue() <= 3) {
+                    return false;
+                }
+            }
+        }
+
+        if (getAverageScore() < 4.75) {
+            return false;
+        }
+
+        if (qualificationWorkDone && qualificationWorkMark.intValue() < 5) {
+            return false;
+        }
+
+        return true;
     }
 }
