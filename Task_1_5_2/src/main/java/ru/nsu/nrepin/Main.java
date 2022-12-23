b3511d3c6dddac3e96e359d7d660cfe1a64baef0
Nikolay Repin <nikolat-repin201303@mail.ru>
2022-12-23 23:18:27 +0700

    Add tests and javadocs

--- Task_1_5_2/src/main/java/ru/nsu/nrepin/Main.java
+++ Task_1_5_2/src/main/java/ru/nsu/nrepin/Main.java
@@ -2,7 +2,16 @@ package ru.nsu.nrepin;
 
 import org.apache.commons.cli.CommandLine;
 
+/**
+ * Application main class.
+ */
 public class Main {
+
+    /**
+     * Application entry point.
+     *
+     * @param args application arguments
+     */
     public static void main(String[] args) {
 
         CommandLine cmd = ArgumentParser.parseArguments(args);
--- Task_1_5_2/src/main/java/ru/nsu/nrepin/Record.java
+++ Task_1_5_2/src/main/java/ru/nsu/nrepin/Record.java
@@ -2,12 +2,21 @@ package ru.nsu.nrepin;
 
 import java.time.LocalDateTime;
 
+/**
+ * This class represents single record (note) in notebook.
+ */
 public class Record {
 
     private final LocalDateTime creationTime;
     private final String name;
     private final String text;
 
+    /**
+     * Creates new {@code Record} object with given name and text.
+     *
+     * @param name name of new record
+     * @param text text of new record
+     */
     public Record(String name, String text) {
         this.name = name;
         this.text = text;
@@ -15,20 +24,42 @@ public class Record {
         this.creationTime = LocalDateTime.now();
     }
 
+    /**
+     * Creates new {@code Record} object with given name, text and timestamp.
+     *
+     * @param creationTime timestamp of new record
+     * @param name name of new record
+     * @param text text of new record
+     */
     public Record(LocalDateTime creationTime, String name, String text) {
         this.creationTime = creationTime;
         this.name = name;
         this.text = text;
     }
 
+    /**
+     * Returns timestamp of current record.
+     *
+     * @return timestamp of current record
+     */
     public LocalDateTime getCreationTime() {
         return creationTime;
     }
 
+    /**
+     * Returns name of current record.
+     *
+     * @return name of current record
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns text of current record.
+     *
+     * @return text of current record
+     */
     public String getText() {
         return text;
     }
