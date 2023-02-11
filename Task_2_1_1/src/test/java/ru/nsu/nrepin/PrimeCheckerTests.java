a0366c4888376f5ce5c834603c81b78af9e5c419
Nikolay Repin <nikolat-repin201303@mail.ru>
2023-02-12 03:22:38 +0700

    Add sequential test

--- Task_2_1_1/src/test/java/ru/nsu/nrepin/PrimeCheckerTests.java
+++ Task_2_1_1/src/test/java/ru/nsu/nrepin/PrimeCheckerTests.java
@@ -3,11 +3,26 @@ package ru.nsu.nrepin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class PrimeCheckerTests {
 
     @Test
     void testIsPrime() {
         Assertions.assertTrue(PrimeChecker.isPrime(3));
         Assertions.assertFalse(PrimeChecker.isPrime(6));
+    }
+
+    @Test
+    void testSequential() {
+        List<Integer> numbers = List.of(1, 3, 5, 7, 11, 13, 17, 23, 31);
+
+        PrimeChecker checker = new SequentalPrimeChecker();
+
+        Assertions.assertFalse(checker.checkList(numbers));
+
+        numbers = List.of(1, 3, 5, 6, 7, 11);
+
+        Assertions.assertTrue(checker.checkList(numbers));
     }
 }
