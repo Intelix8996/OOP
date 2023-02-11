64b391bbce7bcc79c31b8726638016908ad2d113
Intelix8996 <Intelix8996@users.noreply.github.com>
2023-02-11 20:23:36 +0000

    Deploying to gh-pages from @ Intelix8996/OOP@c2d8f00a177867572022b083a965e61a6b55949a 🚀

---
+++ Task_2_1_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.nrepin","l":"Main"},{"p":"ru.nsu.nrepin","l":"ParallelStreamPrimeChecker"},{"p":"ru.nsu.nrepin","l":"PrimeChecker"},{"p":"ru.nsu.nrepin","l":"PrimeCheckerThread"},{"p":"ru.nsu.nrepin","l":"SequentalPrimeChecker"},{"p":"ru.nsu.nrepin","l":"ThreadPrimeChecker"}]
