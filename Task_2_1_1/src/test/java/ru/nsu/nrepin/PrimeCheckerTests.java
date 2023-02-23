2add919a251d87d8b38bbdb20937dadf80ed7543
Nikolay Repin <nikolat-repin201303@mail.ru>
2023-02-23 23:58:09 +0700

    Add tests

--- Task_2_1_1/src/test/java/ru/nsu/nrepin/PrimeCheckerTests.java
+++ Task_2_1_1/src/test/java/ru/nsu/nrepin/PrimeCheckerTests.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class PrimeCheckerTests {
 
+    private static final List<Integer> numbersFalse = List.of(1, 3, 5, 7, 11, 13, 17, 23, 31);
+    private static final List<Integer> numbersTrue = List.of(1, 3, 5, 6, 7, 11);
+
     @Test
     void testIsPrime() {
         Assertions.assertTrue(PrimeChecker.isPrime(3));
@@ -15,14 +18,39 @@ public class PrimeCheckerTests {
 
     @Test
     void testSequential() {
-        List<Integer> numbers = List.of(1, 3, 5, 7, 11, 13, 17, 23, 31);
-
         PrimeChecker checker = new SequentalPrimeChecker();
 
-        Assertions.assertFalse(checker.checkList(numbers));
+        Assertions.assertFalse(checker.checkList(numbersFalse));
 
-        numbers = List.of(1, 3, 5, 6, 7, 11);
+        Assertions.assertTrue(checker.checkList(numbersTrue));
+    }
 
-        Assertions.assertTrue(checker.checkList(numbers));
+    @Test
+    void testParallelStream() {
+        PrimeChecker checker = new ParallelStreamPrimeChecker();
+
+        Assertions.assertFalse(checker.checkList(numbersFalse));
+
+        Assertions.assertTrue(checker.checkList(numbersTrue));
+    }
+
+    @Test
+    void testSingleThreaded() {
+        PrimeChecker checker = new ThreadedPrimeChecker(1);
+
+        Assertions.assertFalse(checker.checkList(numbersFalse));
+
+        Assertions.assertTrue(checker.checkList(numbersTrue));
+    }
+
+    @Test
+    void testMultiThreaded() {
+        int coreCount = Runtime.getRuntime().availableProcessors();
+
+        PrimeChecker checker = new ThreadedPrimeChecker(coreCount);
+
+        Assertions.assertFalse(checker.checkList(numbersFalse));
+
+        Assertions.assertTrue(checker.checkList(numbersTrue));
     }
 }
